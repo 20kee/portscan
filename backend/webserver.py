16398dbaa5ee74320c79b0c98f26d18634c40265
y0x79min <y0x79min@gamil.com>
2023-10-30 18:30:13 +0900

    ip받는부분 추가 webserver.py

--- backend/webserver.py
+++ backend/webserver.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='../frontend')
 
-@app.route('/')
+@app.route("/",  methods=['GET', 'POST'])
 def index():
-    return render_template('templates/index.html')
+    ipaddr=""
+ 
+    if request.method == 'POST':
+        ipaddr = request.form.get("ip")
+    return render_template('templates/index.html',ipaddr=ipaddr)
 
 if __name__ == '__main__':
     app.run(debug=True, host = '192.168.1.169', port=80)
