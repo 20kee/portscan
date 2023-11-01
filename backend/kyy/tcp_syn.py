37376b1a540ab58c58da1f41cf8f09faf8d23fe0
[HyunsiU] <blackfire_@naver.com>
2023-11-01 17:12:58 +0900

    느린 tcp_syn 업로드

--- backend/kyy/tcp_syn.py
+++ backend/kyy/tcp_syn.py
@@ -1,31 +1,30 @@
 from scapy.all import *
+import socket
 
 
-def is_open(ip, port, re1, timeout=0.2):
-    
-    tcpRequest = IP(dst=ip)/ TCP(sport=RandShort(), dport=port, flags='S')
-    tcpResponese = sr1(tcpRequest, timeout = timeout, verbose = 0)
+def scan(dst, port, flag):
     try:
-        if tcpResponese.getlayer(TCP).flags == "SA":
-            #print(tcpRequest.getlayer(TCP).dport)
-            re1.append(tcpRequest.getlayer(TCP).dport)
-            print(tcpResponese)
-    except AttributeError:
-        pass
+        src_port = RandShort()    
+        ip = IP(dst=dst)
+        tcp = TCP(sport = src_port, dport = port, flags=flag.upper())
 
-def thread(ip, sp, ep):
-    re1 = list()
-    for i in range(sp, ep):
-        thread_1 = threading.Thread(target=is_open, args=(ip, i, re1))
-        thread_1.start()
-    print(re1)
-    
-    
-        
+        packet = ip /tcp
+        response = sr1(packet, timeout=1, verbose = 0)
+
+        if response is not None :
+            if response.haslayer(TCP):
+                if response[TCP].flags == 0x12:     # syn, ack 로 답 오기 떄문에 0x12
+                    print(response)
+                elif response[TCP].flags == 0x14:   # rst, ack 로 답오기 떄문
+                    print(f'{port} port is close')
+        elif flag.upper().find('F') != -1:          # fin, xmas 스캔 일경우 모두 FIN flag를 사룡함
+            print(f'{port} port is open')
+        elif not flag :                             # null scan
+            print(f'{port} port is open')
+    except ValueError :
+        print('flag 값을 잘못 입력 했습니다. ex) S(SYN), A(ACK), F(FIN), R(RST), P(PSH), U(URG)')
+
 
 if __name__ == '__main__':
-    ip="13.209.19.147"
-    start = time.time()
-    thread(ip,0,4000)
-    end = time.time()
-    print(end-start)
+    #'13.209.19.147'
+    scan('13.209.19.147', 2, 'f')
