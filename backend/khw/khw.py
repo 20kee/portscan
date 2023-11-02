2abce632f8632df9e0ddec65612a181eee2d457d
76embiid21 <76embiid21@naver.com>
2023-11-02 10:08:27 +0900

    11.02

--- backend/khw/khw.py
+++ backend/khw/khw.py
@@ -1,29 +1,33 @@
 import asyncio
-import time
+
 TARGET_IP = "3.142.251.166"
 PORT_RANGE = (1, 65535)
 CONCURRENT_TASKS = 5000
 TIMEOUT = 0.5  # seconds
-BUFFER_SIZE = 1024
 
 SERVICE_IDENTIFIERS = {
-    21: "FTP",
-    22: "SSH",
-    23: "Telnet",
-    25: "SMTP",
-    80: "HTTP",
-    443: "HTTPS",
+    21: ("FTP", "USER anonymous\r\n"),
+    22: ("SSH", ""),
+    23: ("Telnet", ""),
+    25: ("SMTP", "EHLO localhost\r\n"),
+    80: ("HTTP", "GET / HTTP/1.1\r\nHost: {}\r\n\r\n".format(TARGET_IP)),
+    443: ("HTTPS", ""),
     # ... 추가할 서비스와 포트 번호
 }
 
 async def check_port(ip: str, port: int) -> (bool, str):
-    conn = asyncio.open_connection(ip, port)
     try:
-        reader, writer = await asyncio.wait_for(conn, timeout=TIMEOUT)
-        banner = await reader.read(BUFFER_SIZE)
+        reader, writer = await asyncio.wait_for(asyncio.open_connection(ip, port), timeout=TIMEOUT)
+        
+        service_info = SERVICE_IDENTIFIERS.get(port)
+        if service_info and service_info[1]:
+            writer.write(service_info[1].encode())
+            await writer.drain()
+
+        banner = await asyncio.wait_for(reader.read(1024), timeout=TIMEOUT)
         writer.close()
-        await writer.wait_closed()
         return True, banner.decode(errors='replace').strip()
+
     except:
         return False, ""
 
@@ -35,7 +39,7 @@ async def port_scanner(ip: str, start_port: int, end_port: int):
         results = await asyncio.gather(*tasks)
         for port, (is_open, banner) in zip(range(start_port, end_port + 1), results):
             if is_open:
-                service = SERVICE_IDENTIFIERS.get(port, "Unknown")
+                service = SERVICE_IDENTIFIERS.get(port, ("Unknown",))[0]
                 open_ports[port] = {"service": service, "banner": banner}
 
     for current_port in range(start_port, end_port + 1):
@@ -51,8 +55,5 @@ async def port_scanner(ip: str, start_port: int, end_port: int):
     return open_ports
 
 if __name__ == "__main__":
-    start = time.time()
     result = asyncio.run(port_scanner(TARGET_IP, *PORT_RANGE))
-    for port, data in result.items():
-        print(f"Port: {port}, Service: {data['service']}, Banner: {data['banner']}")
-    print(time.time()- start)
+    # 출력 생략
