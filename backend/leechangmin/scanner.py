e265613a21bc07c639525f44052a8a37a634ee92
20kee <20kee@naver.com>
2023-11-07 15:13:13 +0900

    half-open scan update

--- backend/leechangmin/scanner.py
+++ backend/leechangmin/scanner.py
@@ -1,5 +1,7 @@
 #기본적인 TCP 포트 스캔
- 
+from scapy.all import sr, IP, TCP
+import time
+import socket
 import socket
 import threading
 import multiprocessing
@@ -11,8 +13,19 @@ import copy
 class NormalScanner:
     def __init__(self):
         self._results = {}
-        self._send_messages = dict()
-        self._send_messages[80] = "GET / HTTP/1.1\r\n\r\n"
+        self._open_ports = []
+
+    def half_open_scan(self, ip, start_port, end_port):
+        packet = IP(dst=ip)
+        packet /= TCP(dport=range(start_port, end_port), flags="S")
+        answered, unanswered = sr(packet, timeout=1)
+
+        for (send, recv) in answered:
+            flags = recv.getlayer("TCP").sprintf("%flags%")
+            if flags == "SA":
+                self._open_ports.append(send.dport)
+
+        return self._open_ports
 
     def scan(self, ip, start_port, end_port, fast=True):
         manager = multiprocessing.Manager()
@@ -32,7 +45,7 @@ class NormalScanner:
         result = manager.dict()
         
         for i in range(process_number):
-            processes.append(multiprocessing.Process(target=self.scan_process, args=(ip, ports[i], ports[i+1]-1, result)))
+            processes.append(multiprocessing.Process(target=self.scan_process, args=(ip, range(ports[i], ports[i+1]), result)))
             processes[i].start()
         for p in processes:
             p.join()
@@ -40,11 +53,10 @@ class NormalScanner:
         return json.dumps(dict(result))
         
     
-    def scan_process(self, ip, start_port, end_port, result):
-        port_count = end_port - start_port + 1 
-        task_number = port_count // 2500 + 1
+    def scan_process(self, ip, ports, result):
+        task_number = len(ports) // 2500 + 1
         threads = []
-        for i in range(start_port, end_port, task_number):
+        for i in range(ports[0], ports[-1], task_number):
             t = threading.Thread(target=self.work, args=(ip, i, i+task_number-1, result))
             t.start()
             threads.append(t)
@@ -80,12 +92,14 @@ class NormalScanner:
 def main():
     url = 'github.com'
     ip = socket.gethostbyname(url) if url != '' else '142.250.31.105'
+    result = list()
     scanner = NormalScanner()
-
-    print('========== Fast Version ==========')
-    start_time = time.time()
-    print(scanner.scan(ip, 1, 5000))
-    print('Execution Time :', time.time() - start_time)
+    open_ports = scanner.half_open_scan(ip, 1, 5000)
+    print(open_ports)
+    # print('========== Fast Version ==========')
+    # start_time = time.time()
+    # print(scanner.scan(ip, 1, 5000))
+    # print('Execution Time :', time.time() - start_time)
     
 if __name__ == '__main__':
     main()
